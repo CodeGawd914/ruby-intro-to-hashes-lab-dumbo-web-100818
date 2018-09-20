f8d30fae3282750869619c438ef2447487749164
Travis Desir <travisgdesir31@gmail.com>
2018-09-20 14:56:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,6 @@ def monopoly_with_third_tier
 	    :rent_in_dollars => {}}}
 end
 
-		
-end
-
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 	
