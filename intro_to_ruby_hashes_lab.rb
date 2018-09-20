61498b98ebf2e93a0717a11ee8f058bf07575093
Travis Desir <travisgdesir31@gmail.com>
2018-09-20 15:09:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :b_and_o => {}
+	      :b_and_o => {},
 	      :shortline => {}
 	    },
 	    :rent_in_dollars => {
