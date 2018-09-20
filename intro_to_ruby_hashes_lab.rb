d4140f822d0f44d6e4fc5be6a5fbb68e7e5cbdaf
Travis Desir <travisgdesir31@gmail.com>
2018-09-20 15:22:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,19 +35,20 @@ end
 def monopoly_with_fourth_tier
 		monopoly = {
 	  :railroads => {
-	    :pieces => 4, 
-	    :names => {
-	      :reading_railroad => {},
-	      :pennsylvania_railroad => {},
-	      :b_and_o => {},
-	      :shortline => {}
+	    :pieces => 4,
+	    }, 
+	  :names => {
+	    :reading_railroad => {},
+	    :pennsylvania_railroad => {},
+	    :b_and_o => {},
+	    :shortline => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
 	      :three_pieces_owned => 100,
 	      :four_pieces_owned => 200
-	    }}}
+	    }}
 end
 
 
